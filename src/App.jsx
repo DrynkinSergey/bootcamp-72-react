e067baae5bcd80d2d7ca0a0adb0642d42a571aa2
Sergey <sergsmile29@gmail.com>
2024-06-12 09:31:59 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
-import { ColorPicker } from './components/ColorPicker/ColorPicker';
-import { Counter } from './components/Counter/Counter';
-import { TodoList } from './components/TodoList/TodoList';
-import Vote from './components/Vote/Vote';
-
 export const App = () => {
-  return (
-    <div>
-      {/* <Counter /> */}
-      {/* <ColorPicker /> */}
-      <TodoList />
-      <Vote />
-    </div>
-  );
+  return <div>Hello world</div>;
 };
 export default App;
