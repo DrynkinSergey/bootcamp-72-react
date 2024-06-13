fa7062f865dc90f32b0b931bfa36f931e6dc5244
Sergey <sergsmile29@gmail.com>
2024-06-13 09:48:19 +0300

    update m3l1 init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { ColorPicker } from './components/ColorPicker/ColorPicker';
-import { Counter } from './components/Counter/Counter';
-import { TodoList } from './components/TodoList/TodoList';
-import Vote from './components/Vote/Vote';
+import { Employee } from './components/Employees/Employee';
+import Form from './components/Forms/Form';
 
 export const App = () => {
   return (
-    <div>
-      {/* <Counter /> */}
-      {/* <ColorPicker /> */}
-      <TodoList />
-      <Vote />
-    </div>
+    <>
+      <Form />
+      {/* <Employee /> */}
+    </>
   );
 };
 export default App;
---
+++ src/components/Forms/Form.jsx
@@ -0,0 +1,32 @@
+export const Form = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('Registered!');
+  };
+  return (
+    <div className='formWrapper'>
+      <form className='form' onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        <label className='label'>
+          <span>Name</span>
+          <input className='input' placeholder='Enter your name' type='text' />
+        </label>
+
+        <label className='label'>
+          <span>Email</span>
+          <input className='input' placeholder='Enter your email' type='email' />
+        </label>
+
+        <label className='label'>
+          <span>Password</span>
+          <input className='input' placeholder='Enter your password' type='password' />
+        </label>
+
+        <button className='btn' type='submit'>
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Form;
