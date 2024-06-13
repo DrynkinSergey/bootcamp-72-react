8fd87e10f5bd8dab804bce9b7b332bfa3514d304
Sergey <sergsmile29@gmail.com>
2024-06-14 01:54:01 +0300

    m3l2 init

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,27 +4,16 @@ import { motion } from 'framer-motion';
 
 const Modal = ({ children, title = 'Default modal', onClose }) => {
   useEffect(() => {
-    console.log('Модальне вікно відкрилось!');
     const handleKeyDown = e => {
-      console.log(e.key);
       if (e.key === 'Escape') {
         onClose();
       }
     };
 
     document.addEventListener('keydown', handleKeyDown);
-    const intervalId = setInterval(() => {
-      console.log(new Date().toLocaleTimeString());
-    }, 1000);
-    const timeoutId = setTimeout(() => {
-      console.log('Woohoo!');
-    }, 3000);
 
     return () => {
-      console.log('Модальне вікно закривається!');
       document.removeEventListener('keydown', handleKeyDown);
-      clearInterval(intervalId);
-      clearTimeout(timeoutId);
     };
   }, [onClose]);
 
