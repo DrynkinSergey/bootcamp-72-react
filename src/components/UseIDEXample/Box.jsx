90e9e05909891df80459a68f74b3b7ed1bf66463
Sergey <sergsmile29@gmail.com>
2024-06-16 23:24:31 +0300

    init m4l1

--- src/components/UseIDEXample/Box.jsx
+++
@@ -1,6 +0,0 @@
-export const Box = ({ index }) => {
-  const getRandomColor = () => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  };
-  return <div style={{ width: '100px', height: '100px', backgroundColor: index % 2 === 0 ? 'green' : 'black' }}></div>;
-};
