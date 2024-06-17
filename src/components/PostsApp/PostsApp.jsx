552a26c619e7b10ef97084bdfc5e14c04779e1b5
Sergey <sergsmile29@gmail.com>
2024-06-17 12:16:18 +0300

    searchByQuery

--- src/components/PostsApp/PostsApp.jsx
+++ src/components/PostsApp/PostsApp.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getPosts } from '../../services/api';
+import { getPosts, getPostsByQuery } from '../../services/api';
 import s from './PostsApp.module.scss';
 import { List } from './List';
 import { SearchBar } from './SearchBar';
@@ -9,13 +9,16 @@ export const PostsApp = () => {
   const [skip, setSkip] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [query, setQuery] = useState('');
+  const [total, setTotal] = useState(0);
   useEffect(() => {
     const getData = async () => {
       try {
         setIsLoading(true);
         setError(false);
-        const { posts } = await getPosts({ skip });
+        const { posts, total } = query ? await getPostsByQuery({ q: query, skip }) : await getPosts({ skip });
         setPosts(prev => [...prev, ...posts]);
+        setTotal(total);
       } catch (error) {
         setError(error);
       } finally {
@@ -24,15 +27,25 @@ export const PostsApp = () => {
     };
     getData();
     //IIFE =  Immediate Invoked Function Expression
-  }, [skip]);
+  }, [skip, query]);
+
+  const handleSetQuery = query => {
+    setQuery(query);
+    setPosts([]);
+    setSkip(0);
+  };
 
   return (
     <div className={s.wrapper}>
-      <SearchBar />
+      <SearchBar setQuery={handleSetQuery} />
+      {query && <h2>Search query: {query}</h2>}
+
       <List posts={posts} />
+
       {isLoading && <Loader />}
       {error && <div>Something went wrong</div>}
-      {!isLoading && (
+      {!isLoading && !posts.length && <div>By query `{query}` not found posts</div>}
+      {!isLoading && posts.length < total && (
         <div className={s.loadMore}>
           <button className='btn' onClick={() => setSkip(prev => prev + 6)}>
             Load more
--- src/components/PostsApp/SearchBar.jsx
+++ src/components/PostsApp/SearchBar.jsx
@@ -1,3 +1,15 @@
-export const SearchBar = () => {
-  return <div></div>;
+import { useForm } from 'react-hook-form';
+import s from './PostsApp.module.scss';
+export const SearchBar = ({ setQuery }) => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    setQuery(data.query);
+  };
+  return (
+    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder='Search...' className={s.input} type='search' {...register('query')} />
+      <button className='btn'>Search</button>
+    </form>
+  );
 };
