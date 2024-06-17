0c3abeaf37ece3b7451be35e7efb30cab8f398da
Sergey <sergsmile29@gmail.com>
2024-06-17 11:42:26 +0300

    loading, error

---
+++ src/components/PostsApp/Loader.jsx
@@ -0,0 +1,18 @@
+import { Comment } from 'react-loader-spinner';
+import s from './PostsApp.module.scss';
+export const Loader = () => {
+  return (
+    <div className={s.loader}>
+      <Comment
+        visible={true}
+        height='200'
+        width='200'
+        ariaLabel='comment-loading'
+        wrapperStyle={{}}
+        wrapperClass=''
+        color='#fff'
+        backgroundColor='#F4442E'
+      />
+    </div>
+  );
+};
--- src/components/PostsApp/PostsApp.jsx
+++ src/components/PostsApp/PostsApp.jsx
@@ -3,28 +3,42 @@ import { getPosts } from '../../services/api';
 import s from './PostsApp.module.scss';
 import { List } from './List';
 import { SearchBar } from './SearchBar';
+import { Loader } from './Loader';
 export const PostsApp = () => {
   const [posts, setPosts] = useState([]);
   const [skip, setSkip] = useState(0);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const getData = async () => {
-      const { posts } = await getPosts({ skip });
-      setPosts(prev => [...prev, ...posts]);
+      try {
+        setIsLoading(true);
+        setError(false);
+        const { posts } = await getPosts({ skip });
+        setPosts(prev => [...prev, ...posts]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     };
     getData();
     //IIFE =  Immediate Invoked Function Expression
   }, [skip]);
 
   return (
-    <div>
+    <div className={s.wrapper}>
       <SearchBar />
       <List posts={posts} />
-      <div className={s.loadMore}>
-        <button className='btn' onClick={() => setSkip(prev => prev + 6)}>
-          Load more
-        </button>
-      </div>
+      {isLoading && <Loader />}
+      {error && <div>Something went wrong</div>}
+      {!isLoading && (
+        <div className={s.loadMore}>
+          <button className='btn' onClick={() => setSkip(prev => prev + 6)}>
+            Load more
+          </button>
+        </div>
+      )}
     </div>
   );
 };
