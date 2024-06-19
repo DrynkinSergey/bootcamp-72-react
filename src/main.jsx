ec57d897b251729c9f4cd30696d74996010a5fb6
Sergey <sergsmile29@gmail.com>
2024-06-20 02:22:31 +0300

    init m5l1

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-// import 'modern-normalize';
 import { Toaster } from 'react-hot-toast';
-import { ContextProvider } from './context/ContextProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ContextProvider>
+  <>
     <App />
     <Toaster position='top-right' autoClose={1500} reverseOrder={false} />
-  </ContextProvider>
+  </>
 );
