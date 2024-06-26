9d5ad554e4239a35713e9f1f48b754352771e120
Sergey <sergsmile29@gmail.com>
2024-06-26 03:06:19 +0300

    create

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import { Counter } from './components/Counter/Counter';
-import { TodoListApp } from './components/Todolist/TodoListApp';
-
 export const App = () => {
-  return (
-    <>
-      {/* <Counter /> */}
-      <TodoListApp />
-    </>
-  );
+  return <></>;
 };
--- src/redux/todolist/constants.js
+++
@@ -1,2 +0,0 @@
-export const DELETE_ITEM = 'DELETE_ITEM';
-export const ADD_ITEM = 'ADD_ITEM';
