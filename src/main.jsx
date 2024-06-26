12013e4d7e36861425b1b1c71c58fb5f220aa8a7
Sergey <sergsmile29@gmail.com>
2024-06-26 10:15:52 +0300

    configure redux store

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import './index.css';
 
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-    <Toaster position='top-right' autoClose={1500} reverseOrder={false} />
+    <Provider store={store}>
+      <App />
+      <Toaster position='top-right' autoClose={1500} reverseOrder={false} />
+    </Provider>
   </BrowserRouter>
 );
