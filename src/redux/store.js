501f21a60359b8fdaf19d161a1dfa938e62f4791
Sergey <sergsmile29@gmail.com>
2024-06-27 10:26:32 +0300

    middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,33 @@ import storage from 'redux-persist/lib/storage';
 import { configureStore } from '@reduxjs/toolkit';
 import { todosReducer } from './todolist/slice';
 import { filterReducer } from './filterSlice';
-
+import logger from 'redux-logger';
+import toast from 'react-hot-toast';
 const persistConfig = {
   key: 'todos',
   version: 1,
   storage,
+};
+const checkCountry = store => next => action => {
+  if (action.payload?.todo === 'test') {
+    toast.error('Sorry, but you cant write anything!');
+    return;
+  }
+  next(action);
+};
+
+const myMiddleware = store => next => action => {
+  // console.log(action);
+  if (action.payload?.todo === 'angular') {
+    toast('Angular detected');
+    setTimeout(() => {
+      action.payload.todo = 'REACT';
+      toast('Angular replaced to React');
+      next(action);
+    }, 2000);
+  } else {
+    next(action);
+  }
 };
 
 const persistedReducer = persistReducer(persistConfig, todosReducer);
@@ -23,7 +45,7 @@ export const store = configureStore({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }),
+    }).concat(checkCountry, myMiddleware),
 });
 
 export const persistor = persistStore(store);
