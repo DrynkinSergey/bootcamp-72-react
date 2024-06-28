a1159eda94d7e046981ed793018f4481fdfc0106
Sergey <sergsmile29@gmail.com>
2024-06-28 11:03:50 +0300

    useDeleteTodoMutation

--- src/components/TodolistRTQ/Item.jsx
+++ src/components/TodolistRTQ/Item.jsx
@@ -1,12 +1,17 @@
+import { useDeleteTodoMutation } from '../../redux/todoApi';
+
 export const Item = ({ item }) => {
   const { todo } = item;
+  const [deleteTodo] = useDeleteTodoMutation();
   return (
     <li className='card bg-base-100  shadow-xl'>
       <div className='card-body'>
         <h2 className='card-title'> {todo}</h2>
         <input type='checkbox' checked={item.completed} />
         <div className='card-actions justify-end'>
-          <button className='btn btn-primary'>Delete</button>
+          <button onClick={() => deleteTodo(item.id)} className='btn btn-primary'>
+            Delete
+          </button>
         </div>
       </div>
     </li>
--- src/redux/todoApi.js
+++ src/redux/todoApi.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const todoApi = createApi({
+  tagTypes: ['todos'],
   reducerPath: 'todoApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://667d0cc0297972455f633f0e.mockapi.io/',
@@ -8,6 +9,7 @@ export const todoApi = createApi({
   endpoints: builder => ({
     getTodos: builder.query({
       query: () => 'todos',
+      providesTags: ['todos'],
     }),
     addTodo: builder.mutation({
       query: body => ({
@@ -15,8 +17,16 @@ export const todoApi = createApi({
         method: 'POST',
         body,
       }),
+      invalidatesTags: ['todos'],
+    }),
+    deleteTodo: builder.mutation({
+      query: id => ({
+        url: `todos/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['todos'],
     }),
   }),
 });
 
-export const { useGetTodosQuery, useAddTodoMutation } = todoApi;
+export const { useGetTodosQuery, useAddTodoMutation, useDeleteTodoMutation } = todoApi;
