709efc2bb44933c9f95e02402a68bbf9c55224b1
Sergey <sergsmile29@gmail.com>
2024-06-28 10:53:19 +0300

    useGetTodosQuery

--- src/components/TodolistRTQ/List.jsx
+++ src/components/TodolistRTQ/List.jsx
@@ -1,9 +1,12 @@
+import { useGetTodosQuery } from '../../redux/todoApi';
 import { Item } from './Item';
 
 export const List = () => {
+  const { data, isLoading, isError } = useGetTodosQuery();
   return (
     <ul className='grid grid-cols-4 gap-4'>
-      {[].map(item => (
+      {isLoading && <span className='loading'></span>}
+      {data?.map(item => (
         <Item key={item.id} item={item} />
       ))}
     </ul>
