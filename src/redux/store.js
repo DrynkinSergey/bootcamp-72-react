6910e78c034f5a1acad9abcdf48f720a320ef7e3
Sergey <sergsmile29@gmail.com>
2024-06-28 10:44:49 +0300

    define todoapi inside store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,14 @@ import { counterReducer } from './counter/slice';
 import { configureStore } from '@reduxjs/toolkit';
 import { todosReducer } from './todolist/slice';
 import { filterReducer } from './filterSlice';
+import { todoApi } from './todoApi';
 
 export const store = configureStore({
   reducer: {
     todolist: todosReducer,
     counter: counterReducer,
     filter: filterReducer,
+    [todoApi.reducerPath]: todoApi.reducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(todoApi.middleware),
 });
