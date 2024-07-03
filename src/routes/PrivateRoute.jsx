8b1121a53654cb83e29846a56d6dfb8bbb837d6a
Sergey <sergsmile29@gmail.com>
2024-07-03 10:51:48 +0300

    redirect to same page before update

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../redux/auth/slice';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  return isLoggedIn ? children : <Navigate to='login' />;
+  const location = useLocation();
+  return isLoggedIn ? children : <Navigate to='login' state={location} />;
 };
--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../redux/auth/slice';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const PublicRoute = ({ children }) => {
+  const location = useLocation();
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  return isLoggedIn ? <Navigate to='/' replace /> : children;
+  return isLoggedIn ? <Navigate to={location?.state || '/'} replace /> : children;
 };
